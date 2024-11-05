b76c7c440377b8e47410288d0674c3260427ee77
Mukund <mukundlokhande926@gmail.com>
2024-11-05 09:05:16 +0530

    First Commit

---
+++ SpringBootStandaloneProject/src/main/java/com/SpringBootStandaloneProject/SpringBootStandaloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootStandaloneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStandaloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStandaloneProjectApplication.class, args);
+	}
+
+}
